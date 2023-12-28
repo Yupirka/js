cbf3231ca710af0f84f35e18477b4e1f096fd358
Zakhar <Yupirka@gmail.com>
2023-12-28 21:58:59 +0300

    JS

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,23 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-      b = prompt ('на сколько оцените его?', ''),
-      c = prompt ('Один из последних просмотренных фильмов?', ''),
-      d = prompt ('на сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt ('Один из последних просмотренных фильмов?', ''),
+        b = prompt ('на сколько оцените его?', '');
+        if (a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
+          personalMovieDB.movies[a] = b;
+        } else {
+          console.log ('error')
+          i--
+        }
+}
+if (personalMovieDB.count < 10) {
+  console.log ('просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log ('вы классичечкий зритель')
+} else if (personalMovieDB.count > 30) {
+  console.log ('Вы киноман')
+} else {
+  console.log (" Произошла ошибка")
+}
 console.log (personalMovieDB)
